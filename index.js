59626bc53391bee60c28dfd3f75f4edfdde197ab
kumudin-1903 <kumudinipatil1903@gmail.com>
2025-01-10 11:46:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const nodemailer = require("nodemailer");
+
+const server = http.createServer((request, response) => {
+    const auth = nodemailer.createTransport({
+        service: "gmail",
+        secure : true,
+        port : 465,
+        auth: {
+            user: "kumudinipatil1903@gmail.com",
+            pass: "schc hxfe pzxu fgfe"
+
+        }
+    });
+
+    const receiver = {
+        from : "kumudinipatil1903@gmail.com",
+        to : "nehabhavsar118@gmail.com",
+        subject : "Node Js Mail Testing!",
+        text : "Hello this is a text mail!"
+    };
+
+    auth.sendMail(receiver, (error, emailResponse) => {
+        if(error)
+        throw error;
+        console.log("success!");
+        response.end();
+    });
+    
+});
+
+server.listen(8080);
